1f04f7bee6db48bf5c182b2a2f46bc851dc8e347
fireaenye <42215335+fireaenye@users.noreply.github.com>
2018-08-08 19:16:54 +0300

    Update Program.cs

--- testString/Program.cs
+++ testString/Program.cs
@@ -1,15 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace testString
-{
+
+
     class Program
     {
         static void Main(string[] args)
         {
+            StringBuilder sb = new StringBuilder("Ma string", 100);
+            sb.Insert(9, " make with StringBuilder");
+            Console.WriteLine(sb);
         }
     }
-}
+
